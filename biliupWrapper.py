fa5eac6d2d4310d5d0c500fbcdfff870dfafc7d8
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-09-13 22:20:30 -0700

    fix: lint

--- biliupWrapper.py
+++ biliupWrapper.py
@@ -12,12 +12,12 @@ from cookieformatter import biliup_to_ytbdl_cookie_write2file
 from inacelery import add
 from noxsegutils.extractor import WRAPPER_CONFIG_DIR as CONFIG_DIREC
 from noxsegutils.download import ytbdl
-from noxsegutils.filename import  strip_medianame_out, put_medianame_backin
+from noxsegutils.filename import strip_medianame_out, put_medianame_backin
 
 DEFAULT_SETTINGS = {
     "biliup_routes": ['qn'],
 }
-BILIUP_ROUTE = 'qn' #'kodo'
+BILIUP_ROUTE = 'qn'  # 'kodo'
 RETRY_ROUTES = DEFAULT_SETTINGS['biliup_routes']
 
 
@@ -36,6 +36,7 @@ def cell_stdout(cmd, silent=False, encoding=None):
         p.wait()
     return p.returncode
 
+
 def bilibili_upload(
         globbed,
         media_basename,
@@ -43,13 +44,13 @@ def bilibili_upload(
         description=None,
         episode_limit=180,
         route='qn',
-        useCelery = False):
-    # because my ytbdl template is always "[uploader] title.mp4" I can extract 
+        useCelery=False):
+    # because my ytbdl template is always "[uploader] title.mp4" I can extract
     # out uploader like this and use as a tag:
     keystamps = json.load(open(CONFIG_DIREC, encoding='utf-8'))
     try:
         ripped_from = re.compile(r'\[(.+)\].+').match(media_basename).group(1)
-        #ripped_from = re.findall(r'\[.+\]', media_basename)[0][1:-1]
+        # ripped_from = re.findall(r'\[.+\]', media_basename)[0][1:-1]
         if source is None:
             try:
                 source = keystamps[ripped_from][0]
@@ -72,9 +73,10 @@ def bilibili_upload(
         tags = [ripped_from]
     title = media_basename[:media_basename.rfind('.')][:60]
     # title rework: [歌切][海德薇Hedvika] 20220608的直播歌曲
-    title = '[{}] {}的直播歌曲'.format(tags[0], os.path.splitext(media_basename)[0][-8:])    
+    title = '[{}] {}的直播歌曲'.format(
+        tags[0], os.path.splitext(media_basename)[0][-8:])
     title = media_basename[:media_basename.rfind('.')][:60].replace(
-        ripped_from, tags[0]).replace('【直播回放】','')
+        ripped_from, tags[0]).replace('【直播回放】', '')
     globbed = sorted(globbed)
     globbed_episode_limit = []
     for i in range(len(globbed) // episode_limit + 1):
@@ -90,9 +92,9 @@ def bilibili_upload(
             episode_limit_prefix = ''
         retry = 0
         cmd = [
-                'biliup',
-                'upload',
-            ]
+            'biliup',
+            'upload',
+        ]
         for x in globbed_episode_limit[i]:
             cmd.append(x)
         cmd.append('--copyright=2')
@@ -120,16 +122,17 @@ def bilibili_upload(
                 globbed_episode_limit[i][index] = os.path.join(
                     relocated_dir_on_fail, os.path.basename(item))
             cmd = [
-                    'biliup',
-                    'upload',
-                ]
+                'biliup',
+                'upload',
+            ]
             for x in globbed_episode_limit[i]:
                 cmd.append(x)
             cmd.append('--copyright=2')
             cmd.append('--desc={}'.format(description))
             cmd.append('--tid=31')
             cmd.append('--tag={}'.format(','.join(tags)))
-            cmd.append('--title=[歌切]{}'.format(title[:60] + episode_limit_prefix))
+            cmd.append(
+                '--title=[歌切]{}'.format(title[:60] + episode_limit_prefix))
             cmd.append('--source={}'.format(source))
             cmd.append('-l=' + route)
             logging.info(['deferring', cmd, 'to celery:'])
@@ -137,7 +140,7 @@ def bilibili_upload(
                 json.dump(cmd, f)
             add.delay(json.dumps(cmd))
             return
-        
+
         while cell_stdout(cmd, encoding="utf-8") != 0:
             rescue = []
             for item in globbed_episode_limit[i]:
@@ -158,9 +161,9 @@ def bilibili_upload(
                 raise Exception(
                     'biliup failed for a total of {} times'.format(
                         str(retry)))
-#=================================================================================================================    
-#AIO 一键url to b站上传
-#=================================================================================================================
+# =================================================================================================================
+# AIO 一键url to b站上传
+# =================================================================================================================
 
 
 class InaBiliup():
@@ -176,8 +179,8 @@ class InaBiliup():
         route: str = BILIUP_ROUTE,
         cleanup: bool = True,
         no_biliup: bool = False,
-        ):
-        self.cleanup = True #False
+    ):
+        self.cleanup = True  # False
         self.outdir = outdir
         self.media = media
         self.episode_limit = episode_limit
@@ -195,26 +198,27 @@ class InaBiliup():
             outdir = self.outdir
             if media == '':
                 return
-            logging.info(f'inaseging {media} at ' + \
-                datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            logging.info(f'inaseging {media} at ' +
+                         datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
             os.chdir(outdir)
-            if 'https:' in media: 
-            # use biliup to renew the cookie file, and write to ytdlp netscape format.
+            if 'https:' in media:
+                # use biliup to renew the cookie file, and write to ytdlp netscape format.
                 subprocess.call(['biliup', 'renew'])
                 biliup_to_ytbdl_cookie_write2file()
-                media = ytbdl(media, soundonly='', aria=16)#, outdir = outdir
+                # , outdir = outdir
+                media = ytbdl(media, soundonly='', aria=16)
             if not cell_stdout([
                 'python',
                 'inaseg.py',
                 '--media={}'.format(media),
                 '--outdir={}'.format(outdir),
-                '--soundonly','',
-                '--cleanup']) == 0:
+                '--soundonly', '',
+                    '--cleanup']) == 0:
                 raise BaseException()
             # inaseg failed?
             logging.info(['inaseg completed on', media])
             # shazam 4 thread seems to be fine not triggering a ban
-            shazaming(outdir, media, threads = self.shazam_thread)
+            shazaming(outdir, media)
             stripped_media_names = strip_medianame_out(outdir, media)
             logging.info(['preparing to upload', stripped_media_names])
             bilibili_upload(
@@ -228,15 +232,15 @@ class InaBiliup():
                     os.remove(i)
             else:
                 put_medianame_backin(
-                    stripped_media_names, media, 
-                    hazamed=r'D:\tmp\ytd\convert2music',
+                    stripped_media_names, media,
+                    shazamed=r'D:\tmp\ytd\convert2music',
                     nonshazamed=r'D:\tmp\ytd\extract')
         except KeyboardInterrupt:
             raise
         except BaseException:
             if self.ignore_errors:
                 for i in glob.glob('*.mp4') + glob.glob('*.aria2') + \
-                    glob.glob('*.part'):
+                        glob.glob('*.part'):
                     os.remove(i)
                 # if os.path.isfile(media): os.remove(media)
                 logging.error(f'{media} failed. file is removed in\
@@ -244,11 +248,13 @@ class InaBiliup():
             else:
                 raise
 
-outdir = '/inaseg' #os.getcwd()#r'D:\tmp\ytd\hedvika'
+
+outdir = '/inaseg'  # os.getcwd()#r'D:\tmp\ytd\hedvika'
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='ina music segment')
-    parser.add_argument('--media', type=str, nargs='+', help='file path or weblink')
+    parser.add_argument('--media', type=str, nargs='+',
+                        help='file path or weblink')
     logging.basicConfig(level=logging.DEBUG, handlers=[
         logging.FileHandler('/inaseg/inaseg.log'),
         logging.StreamHandler()
@@ -259,4 +265,3 @@ if __name__ == '__main__':
             f'inaseging {media} at ' +
             datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
         InaBiliup(media=media).run()
-
