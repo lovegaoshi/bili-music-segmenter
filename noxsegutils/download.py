13a4ad7a45135d67868e2b9cfaf861882010b02c
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-09-14 08:26:15 -0700

    Update download.py

--- noxsegutils/download.py
+++ noxsegutils/download.py
@@ -9,39 +9,38 @@ import uuid
 
 COOKIES_LOCATION = ['--cookies', 'ytdlp_cookies.txt']
 
-#fuzzy match! 
+
 def ytbdl(
-    url: str, soundonly: str = '-f bestaudio',
-    outdir: str = tempfile.gettempdir(),
-    aria: int = None) -> str:
-    r = ''# --restrict-filenames
+        url: str, soundonly: str = '-f bestaudio',
+        outdir: str = tempfile.gettempdir(),
+        aria: int = None) -> str:
     fname = None
     uid = uuid.uuid4()
-    #./youtube-dl
+    # ./youtube-dl
     cmd = ['yt-dlp', url, '-o', os.path.join(
         outdir, f"[%(uploader)s] %(title)s %(upload_date)s.{uid}.%(ext)s")]
-    cmd.extend(COOKIES_LOCATION)       
-    if aria is not None: 
+    cmd.extend(COOKIES_LOCATION)
+    if aria is not None:
         cmd.append('--external-downloader')
         cmd.append('aria2c')
         cmd.append('--external-downloader-args')
-        cmd.append('-x {} -s {} -k 1M'.format(str(aria), str(aria)))
+        cmd.append(f"-x {str(aria)} -s {str(aria)} -k 1M")
     if len(soundonly.split(' ')) > 1:
         cmd.extend(soundonly.split(' '))
     logging.info(cmd)
     passed_download = False
     while not passed_download:
         passed_download = True
-        with Popen(cmd, stdout=PIPE, 
-                universal_newlines=True) as p:
+        with Popen(cmd, stdout=PIPE,
+                   universal_newlines=True) as p:
             for line in p.stdout:
                 logging.info(line)
                 if '[download] Destination' in line:
                     fname = line[len('[download] Destination: '):-1]
-                elif 'has already been downloaded' in line: 
-                    fname = line[len('[download] ') : 
-                    -len(' has already been downloaded') - 1]
-                elif '[Merger]' in line: 
+                elif 'has already been downloaded' in line:
+                    fname = line[len('[download] '):
+                                 -len(' has already been downloaded') - 1]
+                elif '[Merger]' in line:
                     fname = line[len('[Merger] Merging formats into "'):-2]
                 elif "error" in line.lower():
                     passed_download = False
@@ -70,6 +69,7 @@ def ytbdl(
     final_name = downloaded_files[0].replace(str(uid), '')
     os.rename(downloaded_files[0], final_name)
     return final_name
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
