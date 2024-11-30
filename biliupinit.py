ecf645af4665eba752f26bf4a0dcb155fbaed78f
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-11-30 05:08:04 +0000

    fix: aarch build

--- biliupinit.py
+++ biliupinit.py
@@ -3,12 +3,20 @@ import tarfile
 import glob
 import os
 
-subprocess.call(
-    r'curl -s https://api.github.com/repositories/437055168/releases/latest \
-| grep "browser_download_url.*x86_64-linux-musl\.tar\.xz" \
-| cut -d : -f 2,3 | tr -d \"  | wget -qi -', shell=True)
-biliup_tar_path = glob.glob('biliupR*')[0]
-tarfile.open(biliup_tar_path).extractall()
-os.remove(biliup_tar_path)
-for file in glob.glob('biliupR*/*'):
-    os.rename(file, os.path.join('/bin', os.path.basename(file)))
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='ina music segment')
+    parser.add_argument(
+        '--system', type=str, default=r'x86_64-linux-musl\.tar\.xz',
+    )
+    args = parser.parse_args()
+    subprocess.call(
+        r'curl -s https://api.github.com/repositories/437055168/releases/latest \
+    | grep "browser_download_url.*"' + args.system + \
+    r'| cut -d : -f 2,3 | tr -d \"  | wget -qi -', shell=True)
+    biliup_tar_path = glob.glob('biliupR*')[0]
+    tarfile.open(biliup_tar_path).extractall()0
+    os.remove(biliup_tar_path)
+    for file in glob.glob('biliupR*/*'):
+        os.rename(file, os.path.join('/bin', os.path.basename(file)))
